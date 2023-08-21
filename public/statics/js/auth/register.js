a9ed0dc38aafedb4f5e5c8a6dd300594568afa0c
Jidraph! <jidraphkimachia1@gmail.com>
2023-08-21 15:42:42 +0300

    landing page and login

---
+++ public/statics/js/auth/register.js
@@ -0,0 +1,4 @@
+const username = document.getElementById("username").value
+const email = document.getElementById("email").value
+const password = document.getElementById("passwd").value
+const data = { full_name: fullName, username, email, password }
